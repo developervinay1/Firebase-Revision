64611fdc487b28bdca8551b5b8355f7d52f6349f
developer-vsandhu <77821789+developervinay1@users.noreply.github.com>
2023-12-15 22:04:13 +0530

    // Update Document in firebase using updateDoc and doc Function

--- src/Components/FirebasePractice.jsx
+++ src/Components/FirebasePractice.jsx
@@ -9,6 +9,7 @@ import {
   query,
   serverTimestamp,
   setDoc,
+  updateDoc,
   where,
 } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
@@ -18,6 +19,12 @@ export default function FirebasePractice() {
   const [title, setTitle] = useState("");
   const [author, setAuthor] = useState("");
   const [docId, setDocId] = useState("");
+
+  // Update Document
+
+  const [documentId, setDocumentId] = useState("");
+  const [newTitle, setNewTitle] = useState("");
+  const [newAuthor, setNewAuthor] = useState("");
 
   // Add Data to Firebase using addDoc
   const adddata = async () => {
@@ -41,6 +48,23 @@ export default function FirebasePractice() {
   const deleleDoc = async () => {
     const docRef = doc(db, "Books", docId);
     await deleteDoc(docRef);
+  };
+
+  // Update Document
+
+  const updateData = async () => {
+    const docRef = doc(db, "Books", `${documentId}`);
+    await updateDoc(docRef, {
+      Title: newTitle,
+      Author: newAuthor,
+      createdAt: serverTimestamp(),
+    })
+      .then(() => {
+        alert("Document Updated");
+      })
+      .catch((err) => {
+        alert(err);
+      });
   };
 
   useEffect(() => {
@@ -125,6 +149,35 @@ export default function FirebasePractice() {
           placeholder="Document Id"
         />
         <button onClick={() => deleleDoc()}>Delete Doc</button>
+      </div>
+
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "20px",
+          marginTop: "20px",
+        }}
+      >
+        <input
+          type="text"
+          placeholder="Document Id"
+          onChange={(e) => setDocumentId(e.target.value)}
+          style={{ padding: "10px" }}
+        />
+        <input
+          type="text"
+          placeholder="Enter New Title"
+          onChange={(e) => setNewTitle(e.target.value)}
+          style={{ padding: "10px" }}
+        />
+        <input
+          type="text"
+          placeholder="Enter New Author"
+          onChange={(e) => setNewAuthor(e.target.value)}
+          style={{ padding: "10px" }}
+        />
+        <button onClick={() => updateData()}>Update Data</button>
       </div>
     </div>
   );
